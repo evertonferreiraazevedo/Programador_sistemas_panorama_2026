09a98a25b09847aeaae24a95409d7dc330b84410
Everton Azevedo <everton-zero@hotmail.com>
2026-07-01 09:27:55 -0300

    Laco For

---
+++ Estrutura de repetição/For/numeroImpares.py
@@ -0,0 +1,6 @@
+for i in range (1, 20, 2):
+    print(i)
+
+for i in range (20):
+    if i % 2 != 0:
+        print (i)           
---
+++ Estrutura de repetição/For/repeticaofor.py
@@ -0,0 +1,11 @@
+# i = 1
+# while i < 10:
+#     print(i)
+#     i += 1
+
+# for i in range(1, 10):
+#     print(i)
+
+num_fim = int(input("Digite um valor final"))
+for i in range(1, num_fim+1):
+    print(i)
