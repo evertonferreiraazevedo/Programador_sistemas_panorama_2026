b554419137795d2005c0b26de5ad217be78c5bec
Everton Azevedo <everton-zero@hotmail.com>
2026-07-27 11:12:33 -0300

    banco de dados cont

--- Banco de dados/primeiro_BD.py
+++ Banco de dados/primeiro_BD.py
@@ -4,24 +4,22 @@ conexao = sqlite3.connect('exemplo.db')
 # Criar um cursor para interagir com o banco de dados
 cursor = conexao.cursor()
 # Criar a tabela Alunos
-# cursor.execute('''CREATE TABLE Alunos (
-#     ID INTEGER PRIMARY KEY,
-#     Nome TEXT NOT NULL,
-#     Idade INTEGER,
-#     Curso TEXT
-# )''')
-# # Confirmar a transação
-# conexao.commit()
-
-# Inserir dados na tabela
-cursor.execute('''
-INSERT INTO Alunos (Nome, Idade, Curso)
-VALUES ('Ana', 21, 'Engenharia'),
-       ('Bruno', 22, 'Direito'),
-       ('Carla', 20, 'Medicina')
-''')
-
-# Confirmar a transação
+cursor.execute('''CREATE TABLE IF NOT EXISTS Alunos (
+    ID INTEGER PRIMARY KEY,
+    Nome TEXT NOT NULL,
+    Idade INTEGER,
+    Curso TEXT
+)''')
 conexao.commit()
+# Inserir dados na tabela
+def inserir_dados(nome, idade, curso):
+    cursor.execute('''    INSERT INTO Alunos (Nome, Idade, Curso)
+    VALUES (?, ?, ?)''', (nome, idade, curso))
+        # Confirmar a transação
+    conexao.commit()
+nome = input("Qual seu nome: ")
+idade = int(input("Qual sua idade: "))
+curso = input("Qual seu curso: ")
+inserir_dados(nome, idade, curso)
 
 
