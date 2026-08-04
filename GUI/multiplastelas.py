d4b58ad2eb715b00143698016351976e8d704c8b
Everton Azevedo <everton-zero@hotmail.com>
2026-08-04 08:29:00 -0300

    multiplas telas parte 2

--- GUI/multiplastelas.py
+++ GUI/multiplastelas.py
@@ -49,7 +49,6 @@
 # root.mainloop()
 
 import tkinter as tk
-
 def mudar():
     if tela1.winfo_viewable():
         tela1.pack_forget()
@@ -57,17 +56,18 @@ def mudar():
     else:
         tela2.pack_forget()
         tela1.pack(fill="both", expand=True)
-
 janela = tk.Tk()
-
-tela1 = tk.Frame(janela,  bg="red")
-tk.Label(tela1, text="Tela 1").pack()
-tk.Button(tela1, text="Mudar", command=mudar).pack()
-
-tela2 = tk.Frame(janela)
-tk.Label(tela2, text="Tela 2").pack()
-tk.Button(tela2, text="Mudar", command=mudar).pack()
-
+janela.geometry("300x300")
+janela.resizable(False, False)
+tela1 = tk.Frame(janela,  bg="pink")
+label1 = tk.Label(tela1, text="Tela 1")
+label1.pack(pady=10)
+botao1 = tk.Button(tela1, text="Mudar", command=mudar)
+botao1.pack(pady=10)
+tela2 = tk.Frame(janela, bg="lightblue")
+label2 = tk.Label(tela2, text="Tela 2")
+label2.pack(pady=10)
+botao2 = tk.Button(tela2, text="Mudar", command=mudar)
+botao2.pack(pady=10)
 tela1.pack(fill="both", expand=True)
-
 janela.mainloop()
